1f4fdd55c973acd0d81595d120d5973664d7578b
lee-leonardo <leoleez@hotmail.com>
2014-08-04 20:52:23 -0700

    Code Clean up, just some moving around, added pragma marks to delineate the structure. Also fixed some settings in the UIImage.

--- ImageFilterApp/ViewController.swift
+++ ImageFilterApp/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 	var photoPicker = UIImagePickerController()
 	var imageActionSheet = UIAlertController()
 	
+//MARK: IBAction
+	@IBAction func actionSheet(sender: AnyObject) {
+		self.presentViewController(imageActionSheet, animated: true, completion: nil)
+		
+	}
+
+//MARK: Viewcontroller
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -29,23 +36,16 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 				})
 		}
 	}
-
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated)
 		setupPickers()
 		setupActionController()
 	}
-	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 	}
-	
-//MARK: ViewController Methods
-	@IBAction func actionSheet(sender: AnyObject) {
-		self.presentViewController(imageActionSheet, animated: true, completion: nil)
-		
-	}
-	
+
+//MARK: Setup methods
 	func setupPickers() {
 		self.photoPicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
 		self.photoPicker.allowsEditing = true
@@ -54,6 +54,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 		if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
 			self.cameraPicker.sourceType = UIImagePickerControllerSourceType.Camera
 		} else {
+			//This is the default value of UIImagePickerControllerSourceType.
 			self.cameraPicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
 		}
 		self.cameraPicker.allowsEditing = true
@@ -73,36 +74,30 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 			//assetsLibrary.enumerateGroupsWithTypes(types: ALAssetsGroupType, usingBlock: ALAssetsLibraryGroupsEnumerationResultsBlock?, failureBlock: ALAssetsLibraryAccessFailureBlock?)
 		}
 	}
-
 	
-//MARK: UIImagePickerController
-	func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
-		var imageReturn = info[UIImagePickerControllerEditedImage] as UIImage
-		self.imageView.image = imageReturn
-		self.dismissViewControllerAnimated(true, completion: nil)
-	}
+//MARK: ActionSheet
 	func setupActionController() {
 		imageActionSheet = UIAlertController(title: "Pick Source", message: "This will allow one to pick a source for the Image", preferredStyle: UIAlertControllerStyle.ActionSheet)
 		
 		let cameraButton = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default, handler: {
-		(action : UIAlertAction!) -> Void in
-		
+			(action : UIAlertAction!) -> Void in
+			
 			if self.cameraPicker.sourceType == UIImagePickerControllerSourceType.Camera {
 				self.presentViewController(self.cameraPicker, animated: true, completion: nil)
 			} else {
 				var noCameraAlert = UIAlertController(title: "No Camera on Device", message: "This device does not have a camera for this app to use.", preferredStyle: UIAlertControllerStyle.Alert)
-			let cancel = UIAlertAction(title: "Okay", style: UIAlertActionStyle.Cancel, handler: nil)
+				let cancel = UIAlertAction(title: "Okay", style: UIAlertActionStyle.Cancel, handler: nil)
 				noCameraAlert.addAction(cancel)
-			
-			self.presentViewController(noCameraAlert, animated: true, completion: nil)
+				
+				self.presentViewController(noCameraAlert, animated: true, completion: nil)
 			}
-		
-		})
+			
+			})
 		let photoLibrary = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default, handler: {
-		(action: UIAlertAction!) -> Void in
+			(action: UIAlertAction!) -> Void in
 			self.presentViewController(self.photoPicker, animated: true, completion: nil)
-		
-		})
+			
+			})
 		
 		let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
 		
@@ -111,6 +106,15 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 		imageActionSheet.addAction(cancel)
 	}
 	
+//MARK: UIImagePickerController
+	func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
+		var imageReturn = info[UIImagePickerControllerEditedImage] as UIImage
+		self.imageView.image = imageReturn
+		self.dismissViewControllerAnimated(true, completion: nil)
+	}
+
+
+//lazy Instantiation... not polished yet?
 //	lazy var imageActionSheet : UIAlertController = {
 //		var imageActionSheet = UIAlertController(title: "Pick Source", message: "This will allow one to pick a source for the Image", preferredStyle: UIAlertControllerStyle.ActionSheet)
 //		
